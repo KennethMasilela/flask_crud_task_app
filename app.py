4ca9573da6b82226fbf87c816f5e121e6dfe0d21
KennethMasilela <kennethmasilela.githubproject@gmail.com>
2025-05-16 22:46:50 +0200

    deploymeny issue

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 # Flask app config
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "fallback_dev_key")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Database and Migrations
